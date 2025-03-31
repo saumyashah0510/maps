d5a24851c77a55e584a1ccbc1122c266c8a56694
saumyashah0510 <202401472@daiict.ac.in>
2025-03-31 12:39:29 +0530

    Initial commit

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,24 +1,24 @@
-class Solution {
-    public:
-        vector<int> twoSum(vector<int>& nums, int target) {
-            
-            unordered_map<int,int> m; // stores value and index
-    
-            for(int i=0;i<nums.size();i++)
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+
+        unordered_map<int, int> m; // stores value and index
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int diff = target - nums[i];
+
+            // if diff is found it will give uss key of diff
+            // and if not found it will give us last element iterator
+            if (m.find(diff) != m.end())
             {
-                int diff = target - nums[i];
-    
-                // if diff is found it will give uss key of diff 
-                // and if not found it will give us last element iterator
-                if(m.find(diff) != m.end())
-                {
-                    return {m[diff],i};
-                }
-    
-                m[nums[i]] = i;
-    
+                return {m[diff], i};
             }
-            return{};
-    
+
+            m[nums[i]] = i;
         }
-    };
+        return {};
+    }
+};
